7364c8e739d7b5175f4f16c457466b0b329ad70a
xdijlxlr <taurus1989520@gmail.com>
2019-04-20 14:52:58 +0800

    [修改] LoginSuccessHandler

--- src/main/java/com/handler/LoginSuccessHandler.java
+++ src/main/java/com/handler/LoginSuccessHandler.java
@@ -9,12 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class LoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-		System.out.println(authentication);
+		log.info(authentication.toString());
 		super.onAuthenticationSuccess(request, response, authentication);
 	}
 
