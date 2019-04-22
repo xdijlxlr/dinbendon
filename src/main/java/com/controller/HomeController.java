d522107ab59bd20141a42f999a6be3b0a6c2b067
xdijlxlr <taurus1989520@gmail.com>
2019-04-22 22:28:37 +0800

    新增

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.ldap.userdetails.LdapUserDetailsImpl;
@@ -12,30 +16,31 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class HomeController {
 
-    @GetMapping("/")
-    public String index() {
+	@GetMapping("/")
+	public String index() throws FileNotFoundException {
 
-        log.info("Getting UsernamePasswordAuthenticationToken from SecurityContextHolder");
-        UsernamePasswordAuthenticationToken authentication =
-                (UsernamePasswordAuthenticationToken)
-                        SecurityContextHolder.getContext().getAuthentication();
+		log.info("Getting UsernamePasswordAuthenticationToken from SecurityContextHolder");
+		UsernamePasswordAuthenticationToken authentication = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 
-        log.info("Getting principal from UsernamePasswordAuthenticationToken");
-        LdapUserDetailsImpl principal = (LdapUserDetailsImpl) authentication.getPrincipal();
+		log.info("Getting principal from UsernamePasswordAuthenticationToken");
+		LdapUserDetailsImpl principal = (LdapUserDetailsImpl) authentication.getPrincipal();
 
-        log.info("authentication: " + authentication);
-        log.info("principal: " + principal);
+		log.info("authentication: " + authentication);
+		log.info("principal: " + principal);
+		for (int i = 0; i < 10; i++) {
+			i = i - 1; // Noncompliant; counter updated in the body of the loop
+		}
+		FileInputStream fileInputStream = new FileInputStream(new File(""));
+		return "Spring Security + Spring LDAP Authentication Configuration Example";
+	}
 
-        return "Spring Security + Spring LDAP Authentication Configuration Example";
-    }
+	@GetMapping("/managers")
+	public String managers() {
+		return "Hello managers";
+	}
 
-    @GetMapping("/managers")
-    public String managers(){
-        return "Hello managers";
-    }
-
-    @GetMapping("/employees")
-    public String employees(){
-        return "Hello employees";
-    }
+	@GetMapping("/employees")
+	public String employees() {
+		return "Hello employees";
+	}
 }
